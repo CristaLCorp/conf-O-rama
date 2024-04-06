9aa3a9d854438567fee366832e3e0e8f9a843a24
cristalcorp <cristal@cristalcorp.com>
2024-04-06 18:06:31 +0200

    upgrade

--- vim/nvchad.nix
+++ vim/nvchad.nix
@@ -6,7 +6,7 @@
   ];
   
   #home.file.".config/tmux/tmux.conf".source = ./tmux.conf;
-  home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
+  home.file.".config/nvim/init.lua".source = ./init.lua;
 
 
 }
